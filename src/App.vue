b3e19fa59222aff285983312c913a74ae2af41a4
kingujebeh <kingujebeh@gmail.com>
2025-08-19 13:52:16 +0000

    App Page Changed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <h1>African Kingdom!</h1>
-  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
   <nav>
-    <RouterLink to="/">Go to Home</RouterLink> &nbsp;
+    <RouterLink to="/">Go to Home</RouterLink>
+    &nbsp;
     <RouterLink to="/market">Go to Market</RouterLink>
+    &nbsp;
     <RouterLink to="/info">Info</RouterLink>
   </nav>
   <main>

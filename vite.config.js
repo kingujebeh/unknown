887ff537f3bd42ed72b470c81091daf5552ae714
kingujebeh <kingujebeh@gmail.com>
2025-08-21 10:07:52 +0000

    Shop Added

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export async function buildAll() {
     { name: "community", outDir: "dist/community" },
     { name: "pro", outDir: "dist/pro" },
     { name: "store", outDir: "dist/store" },
+    { name: "shop", outDir: "dist/shop" },
     { name: "app", outDir: "dist/app" },
   ];
 

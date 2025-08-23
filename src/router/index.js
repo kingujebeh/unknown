9c8a894c5a90e7a889e2d177b412c85986953adc
kingujebeh <kingujebeh@gmail.com>
2025-08-23 01:11:46 +0000

    Bug Correction Worked ON

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,32 @@
 // src/router/RouterFactory.js
 import { createRouter, createWebHistory } from "vue-router";
 
+// Glob all route files (but they won’t all be imported, just mapped)
+const routeModules = import.meta.glob("./routes/*.js");
+const interfaceModules = import.meta.glob("../interface/*/Index.vue");
+
 export default class RouterFactory {
   constructor(software) {
     this.software = software;
   }
 
   async loadRoutes() {
-    // dynamically import only the matching routes file
-    const { default: iroutes } = await import(
-      /* @vite-ignore */ `./routes/${this.software}.js`
-    );
+    // Ensure file exists in the glob
+    const routeImporter = routeModules[`./routes/${this.software}.js`];
+    if (!routeImporter) {
+      throw new Error(`No routes found for ${this.software}`);
+    }
+    const { default: iroutes } = await routeImporter();
 
     return [
       {
         path: "/",
         redirect: "/home",
-        component: () => import("../layouts/Screen.vue"),
+        component: () => import("@/layouts/Screen.vue"),
         children: [
           {
             path: "/home",
-            component: () =>
-              import(
-                /* @vite-ignore */ `../interface/${this.software}/Index.vue`
-              ),
+            component: interfaceModules[`../interface/${this.software}/Index.vue`],
           },
           ...iroutes,
         ],

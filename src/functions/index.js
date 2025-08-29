a701c9db6015465157fc540e4fedee7db397d3ad
augmentchinedu <augmentchinedu@gmail.com>
2025-08-29 07:16:43 +0000

    Data Logged To Console

--- src/functions/index.js
+++ src/functions/index.js
@@ -3,6 +3,7 @@ import api from "@/api";
 async function getData() {
   try {
     let { data } = await api.get("/data");
+    console.log(data);
     return data;
   } catch (error) {
     console.error(error);

c1d46f29881ed822f4edfdd73e5c99548fb752d4
augmentchinedu <augmentchinedu@gmail.com>
2025-09-11 18:33:42 +0000

    PWA Edit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 
 // --- Base Vite config ---
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [vue(), tailwindcss()],
+    plugins: [vue(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
